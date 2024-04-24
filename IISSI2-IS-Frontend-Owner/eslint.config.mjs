526c1ca9c386a273aae35d4c464b732650aeb7c8
Juan Gil <juagilmar4@alum.us.es>
2024-04-24 19:31:04 +0200

    commit

---
+++ IISSI2-IS-Frontend-Owner/eslint.config.mjs
@@ -0,0 +1,43 @@
+import globals from 'globals'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  ...compat.extends('standard'),
+  pluginReactConfig
+]
+
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/prop-types': 'off'
+  }
+}
